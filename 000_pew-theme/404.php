e3734e644840c92a0c0818cb6aa9c217ae54bd96
SquembDev Three <juan@squembri.com>
2017-08-29 10:40:59 +0200

    Añadimos la página de 404
    Añadimos la página 404 con los estilos .less y agregamos el @import en style.less

---
+++ 000_pew-theme/404.php
@@ -0,0 +1,9 @@
+<?php get_header(); ?>
+<div class="container container-error404">
+	<div class="col-md-12">
+		<h2>Error 404 - Página no encontrada.</h2>
+		<p>¡Vaya! Parece que la página que buscas no existe :( </p>
+		<a href="<?php echo get_home_url(); ?>">Volver a la home</a>
+	</div>
+</div>
+<?php get_footer(); ?>
